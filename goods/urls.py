4b1db0540a1ea4cda982d66e5322857c985aebbb
Niferoo <av.pasmurnov@gmail.com>
2024-02-10 14:41:53 +0600

    goods app/ category and products db ready. admin panel ready

---
+++ goods/urls.py
@@ -0,0 +1,12 @@
+#urls from GOODS app!!!
+
+from django.contrib import admin
+from django.urls import path
+from goods import views
+
+app_name = 'goods'
+
+urlpatterns = [
+    path('', views.catalog, name='index'),
+    path('product/', views.product, name='product')
+]
