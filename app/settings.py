74dff832a6152b87b9d728a146cf3117085819e3
Niferoo <av.pasmurnov@gmail.com>
2024-02-13 13:31:29 +0600

    registered simple tag for categories

--- app/settings.py
+++ app/settings.py
@@ -137,6 +137,10 @@ INTERNAL_IPS = [
     # ...
 ]
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
