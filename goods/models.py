6ad6974f710bfa2f82b5431bb5ff9c1fac0a6a37
Niferoo <av.pasmurnov@gmail.com>
2024-03-01 17:41:24 +0600

    paginations and filters

--- goods/models.py
+++ goods/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Categories(models.Model):
     name = models.CharField(max_length= 150, unique = True, verbose_name = "Название")
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name = "URL")
@@ -18,11 +18,12 @@ class Products(models.Model):
 
     name = models.CharField(max_length=150, unique=True, verbose_name="Название")
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
-    description = models.TextField(verbose_name="Описание", blank=True, null=True)
-    image = models.ImageField(verbose_name="Изображение", upload_to="goods_images", blank=True, null=True)
-    price = models.DecimalField(verbose_name="Цена", default=0.00, max_digits=7, decimal_places=2 )
-    discount = models.DecimalField(verbose_name="Скидка", default=0.00, max_digits=4, decimal_places=2)
-    quantity = models.PositiveIntegerField(verbose_name="Количество", default=0,)
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    need_care = models.TextField(null=True, blank=True, verbose_name="Требуемый уход")
+    image = models.ImageField(upload_to="goods_images", blank=True, null=True,verbose_name="Изображение")
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2,verbose_name="Цена" )
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2,verbose_name="Скидка")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
 
     category = models.ForeignKey(to=Categories, on_delete=models.PROTECT, verbose_name="Категория")
 
@@ -30,6 +31,7 @@ class Products(models.Model):
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'продукты'
+        ordering = ('id',)
 
     def __str__(self):
         return f'{self.name} Количесво - {self.quantity}'
@@ -38,7 +40,7 @@ class Products(models.Model):
         return f'{self.id:05}'
 
     def sell_price(self):
-        if self.discount :
+        if self.discount:
             return round(self.price - ((self.price * self.discount)/100), 2)
 
         return self.price
--- goods/views.py
+++ goods/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from goods.models import Products, Categories
-def catalog(request):
+from django.core.paginator import Paginator
 
-    goods = Products.objects.all()
+
+def catalog(request, category_slug, page=1):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, per_page=3)
+    concurrent_page = paginator.page(page)
 
     context = {
         'title': 'Home - Каталог',
-        'goods': goods,
+        'goods': concurrent_page,
+        'slug_url': category_slug
     }
     return render(request, 'goods/catalog.html', context)
 
 
 def product(request, product_slug):
-
     product_slug = Products.objects.get(slug=product_slug)
 
     context = {
